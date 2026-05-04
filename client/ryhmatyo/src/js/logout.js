1ef6c9bdc2e80dac9b5bdd50e694c7f15192adc5
MikaPoikonen <mikapoi@metropolia.fi>
2026-05-04 19:45:01 +0300

    Remove unnecessary comments from logout.js

--- client/ryhmatyo/src/js/logout.js
+++ client/ryhmatyo/src/js/logout.js
@@ -1,11 +1,8 @@
 
-///////////////////////////////////
-// Logout-nappi
 // Logout-nappi
 document.querySelector("#exit")?.addEventListener("click", () => {
     sessionStorage.clear();
     localStorage.removeItem("token");
     localStorage.removeItem("userId");
-
     window.location.replace("index.html");
 });
